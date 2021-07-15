650260fdd203880fe7acdf18d309d1acd28dbf4a
John Kim <schoolkim@gmail.com>
2021-07-15 18:35:21 -0400

    I wa

--- Client/id-form.js
+++ Client/id-form.js
@@ -1,12 +1,26 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import { SafeAreaView, StyleSheet, TextInput, Pressable, Text} from "react-native";
 import Profile from './profile';
+
+const databaseURL = "https://b-card-c6533-default-rtdb.firebaseio.com/"
+
 function IdForm (){
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [info,setInfo] = useState("");
   const [log_in, logIndone] = useState(false);
-  
 
+
+  useEffect(() => {
+    fetch(`${databaseURL}/Test.json`).then(res => {
+      if(res.status !== 200) {
+        throw new Error(res.statusText);
+      }
+      return res.json()
+    }).then(info=> setInfo(info));
+  });
+ 
   return (log_in ? <Profile /> : (
     <SafeAreaView>
       <TextInput
@@ -19,7 +33,7 @@ function IdForm (){
         onChangeText={(password) => setPassword(password)}
         placeholder="Password"
       />
-      <Pressable style={styles.login} onPress= {() => {alert(username);logIndone(true);}}><Text style={styles.textStyle}>Log in</Text></Pressable>
+      <Pressable style={styles.login} onPress= {() => {alert(info);logIndone(true);}}><Text style={styles.textStyle}>Log in</Text></Pressable>
     </SafeAreaView>
   ) );
 }
